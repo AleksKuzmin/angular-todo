0120aa3a025713ce988239e30cc9be9e0f7968cf
Alex Kuzmin <88483359+AleksKuzmin@users.noreply.github.com>
2022-05-19 10:51:01 +1000

    last one

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -28,4 +28,9 @@ export class TodosComponent implements OnInit {
   deleteTodo(id: number) {
     this.todos = this.todos.filter((v, i) => i !== id);
   }
+
+  addTodo() {
+    this.todos.push({ content: this.inputTodo, completed: false });
+    this.inputTodo = '';
+  }
 }
